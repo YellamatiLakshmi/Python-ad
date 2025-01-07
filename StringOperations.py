0da9a673a39ea9e6271881ae392fede1a6c00e14
YellamatiLakshmi <160896148+YellamatiLakshmi@users.noreply.github.com>
2025-01-07 14:02:36 +0000

    String operations

---
+++ StringOperations.py
@@ -0,0 +1,14 @@
+str1 ="  Beautiful  "
+print(str1.strip( ))  #Remove extra white spaces
+
+
+
+words = ["Hello", "Welcome", "AIML", "Epsilon", "Batch"]
+res = " ".join(words)   #joins the Multiple strings into a single string
+print(res)
+
+
+L1 = ["Apple", "Ball", "Cat"]
+L1.append("Book")
+print(L1)
+print(len(L1))
